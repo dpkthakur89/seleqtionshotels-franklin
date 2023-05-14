cb9194b622eaa16cf70c2b759da58e8e25f33883
Deepak Thakur <deepthak@Deepak-MacBook-Pro.local>
2023-05-14 11:08:32 +0530

    final code commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -15,4 +15,69 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
+  //slider css 
+const rightArrow = document.querySelector('.section.summary-container.cards-container > .default-content-wrapper > p > .icon.icon-rightarrow');
+const leftArrow = document.querySelector('.section.summary-container.cards-container > .default-content-wrapper > p > .icon.icon-leftarrow');
+
+leftArrow.onclick = function() {
+    var myText = "This is left arrow";
+    alert (myText);
+    var x = document.querySelectorAll(".cards.select > ul > li");
+    for (let i = 0; i < x.length; i++) {
+      if (i < 3) {x[i].style.display = "block"}
+      else  {x[i].style.display = "none"}
+      leftArrow.style.display = "none"
+      rightArrow.style.display = "inline-block"
+      
+    }
+    
+};
+
+rightArrow.onclick = function() {
+  var myText = "This is right arrow";
+  alert (myText);
+  var x = document.querySelectorAll(".cards.select > ul > li");
+  for (let i = 0; i < x.length; i++) {
+    
+      if (i < 1) {x[i].style.display = "none"}
+        else  {x[i].style.display = "block"}
+        rightArrow.style.display = "none"
+        leftArrow.style.display = "inline-block"
+
+}
+};
+//slider css 
+const rightsArrow = document.querySelector('.arrow.block > div > div > p > .icon.icon-rightarrow');
+const leftsArrow = document.querySelector('.arrow.block > div > div > p > .icon.icon-leftarrow');
+
+leftsArrow.onclick = function() {
+    var myText = "This is left arrow";
+    alert (myText);
+    var x = document.querySelectorAll(".cards.popular > ul > li");
+    for (let i = 0; i < x.length; i++) {
+      if (i < 4) {x[i].style.display = "block"}
+      else  {x[i].style.display = "none"}
+      leftsArrow.style.display = "none"
+      rightsArrow.style.display = "inline-block"
+      
+    }
+    
+};
+
+rightsArrow.onclick = function() {
+  var myText = "This is right arrow";
+  alert (myText);
+  var x = document.querySelectorAll(".cards.popular > ul > li");
+  for (let i = 0; i < x.length; i++) {
+    
+      if (i < 4) {x[i].style.display = "none"}
+        else  {x[i].style.display = "block"}
+        rightsArrow.style.display = "none"
+        leftsArrow.style.display = "inline-block"
+
+}
+};
+
+
 }
